06a92871835a914a73d75656f3a36f5807e068ee
Fabián Vásquez <fvasquezc23@gmail.com>
2018-09-07 10:39:33 -0500

    Modified JWT content

--- src/OAuthToken.php
+++ src/OAuthToken.php
@@ -4,14 +4,36 @@ namespace Byteam\Spectre;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Signer\Rsa\Sha256;
+use Lcobucci\JWT\Token;
+use League\OAuth2\Server\CryptKey;
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
-use League\OAuth2\Server\Entities\Traits\AccessTokenTrait;
 use League\OAuth2\Server\Entities\Traits\EntityTrait;
 use League\OAuth2\Server\Entities\Traits\TokenEntityTrait;
 
 class OAuthToken extends Model implements AccessTokenEntityInterface
 {
-    use EntityTrait, AccessTokenTrait, TokenEntityTrait;
+    use EntityTrait, TokenEntityTrait;
 
     protected $table = "oauth_access_tokens";
+
+    /**
+     * @param CryptKey $privateKey
+     *
+     * @return Token
+     */
+    public function convertToJWT(CryptKey $privateKey): Token
+    {
+        return (new Builder)
+            ->setAudience($this->getClient()->getIdentifier())
+            ->setId($this->getIdentifier())
+            ->setIssuedAt(time())
+            ->setNotBefore(time())
+            ->setExpiration($this->getExpiryDateTime()->getTimestamp())
+            ->setSubject($this->getUserIdentifier())
+            ->sign(new Sha256(), new Key($privateKey->getKeyPath(), $privateKey->getPassPhrase()))
+            ->getToken();
+    }
 }
